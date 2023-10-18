dc6e70d5ee461fefd5d599e1329cf426b11df8ae
ponyoxa <kmsm805@gmail.com>
2023-10-19 07:00:46 +0900

    multi-wordsにひっかかってエラーが出る部分について、いったん機能をオフにする

--- vue3-todo-app-demo/src/views/todos.vue
+++ vue3-todo-app-demo/src/views/todos.vue
@@ -1,1 +1,25 @@
-<template>TODO一覧</template>
+<template>
+  <h2>TODO一覧</h2>
+  <ul>
+    <li v-for="todo in todoStore.state.todos" :key="todo.id">
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { todoKey } from '@/store/todo'
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const todoStore = inject(todoKey)
+    if (!todoStore) {
+      throw new Error('todoStore is not provided')
+    }
+    return {
+      todoStore
+    }
+  }
+})
+</script>
