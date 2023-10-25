0d8be3eb6ae3dd758dfd9092d9a0621395a67adc
ponyoxa <kmsm805@gmail.com>
2023-10-25 22:17:01 +0900

    ストアのインジェクト、新規作成ページのルーティング追加

--- vue3-todo-app/src/router/index.ts
+++ vue3-todo-app/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import AllTodos from '@/views/allTodos.vue'
+import AddTodo from '@/views/AddTodo.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'All-Todos',
     component: AllTodos
+  },
+  {
+    path: '/new',
+    name: 'AddTodo',
+    component: AddTodo
   }
 ]
 
